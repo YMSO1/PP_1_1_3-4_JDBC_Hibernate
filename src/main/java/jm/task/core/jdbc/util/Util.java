8a563be37497a774b94d75dc48a50041afd4ebd4
YMSO1 <yms01@ya.ru>
2022-09-12 21:17:57 +0300

    Решение задачи 113
    читающие методы не должны проходить в транзакции - т.к. не происходит изменений в бд
    
    поля в классах обявлены по умолчанию а не приватные

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
     private static Connection connection;
+    private static SessionFactory sessionFactory;
 
     public static Connection getConnection() {
         try {
@@ -28,6 +35,25 @@ public class Util {
                 throw new RuntimeException(e);
             }
         }
+    }
 
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+
+                properties.put(Environment.URL, URL);
+                properties.put(Environment.USER, USER);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                configuration.setProperties(properties)
+                        .addAnnotatedClass(User.class);
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
